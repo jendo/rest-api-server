f2c8a8d25a7a670e0aff8cd66844fa94a8af0592
Michal Jenis <jenis.michal@gmail.com>
2025-08-25 16:34:08 +0200

    Implement Customer doctrine entity with related migration

---
+++ project/src/Entity/Customer/Customer.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity\Customer;
+
+use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidInterface;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'customer')]
+#[ORM\UniqueConstraint(name: 'UNIQ_CUSTOMER_EMAIL', columns: ['email'])]
+class Customer
+{
+    private const EMAIL_MAX_LENGTH = 100;
+    private const FIRST_NAME_MAX_LENGTH = 100;
+    private const LAST_NAME_MAX_LENGTH = 100;
+
+    #[ORM\Id]
+    #[ORM\Column(type: "uuid")]
+    private UuidInterface $id;
+
+    #[ORM\Column(type: "string", length: self::EMAIL_MAX_LENGTH)]
+    private string $email;
+
+    #[ORM\Column(type: "string", length: self::FIRST_NAME_MAX_LENGTH)]
+    private string $firstName;
+
+    #[ORM\Column(type: "string", length: self::LAST_NAME_MAX_LENGTH)]
+    private string $lastName;
+
+    public function __construct(
+        string $email,
+        string $firstName,
+        string $lastName
+    ) {
+        $this->id = Uuid::uuid4();
+        $this->setEmail($email);
+        $this->setFirstName($firstName);
+        $this->setLastName($lastName);
+    }
+
+    public function getId(): UuidInterface
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    private function setEmail(string $email): void
+    {
+        assert(mb_strlen($email) <= self::EMAIL_MAX_LENGTH && trim($email) !== '');
+
+        $this->email = $email;
+    }
+
+    private function setFirstName(string $firstName): void
+    {
+        assert(mb_strlen($firstName) <= self::FIRST_NAME_MAX_LENGTH && trim($firstName) !== '');
+
+        $this->firstName = $firstName;
+    }
+
+    public function setLastName(string $lastName): void
+    {
+        assert(mb_strlen($lastName) <= self::LAST_NAME_MAX_LENGTH && trim($lastName) !== '');
+
+        $this->lastName = $lastName;
+    }
+}
