da49346b585105792cd1c7b72d49237826378ba1
Michal Jenis <jenis.michal@gmail.com>
2025-08-27 11:28:55 +0200

    Create customer fixtures

---
+++ project/src/DataFixtures/CustomerFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataFixtures;
+
+use App\Entity\Customer\Customer;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CustomerFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $customer1 = new Customer(
+            'jan.novak@example.com',
+            'Jan',
+            'Novák'
+        );
+        $manager->persist($customer1);
+
+        $customer2 = new Customer(
+            'eva.kovacova@example.com',
+            'Eva',
+            'Kováčová'
+        );
+        $manager->persist($customer2);
+
+        $manager->flush();
+    }
+}
