87748bfb47a887daacac94fb5ad9a410b381b9e2
Michal Jenis <jenis.michal@gmail.com>
2025-09-01 16:02:06 +0200

    Unified error response implementation

---
+++ project/src/Api/Response/ResponseFactory.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Api\Response;
+
+use InvalidArgumentException;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class ResponseFactory
+{
+    public const DATA_FIELD_MESSAGE = 'message';
+
+    /**
+     * @param string|array<int, array{message: string, ...}>|array{message: string, ...} $data
+     * @param int $code
+     * @param array<string, string> $headers
+     * @return JsonResponse
+     */
+    public static function error(
+        string|array $data,
+        int $code = Response::HTTP_BAD_REQUEST,
+        array $headers = []
+    ): JsonResponse {
+        if (true === is_string($data) && '' === trim($data)) {
+            throw new InvalidArgumentException('Response must contain some data.');
+        }
+
+        if ([] === $data) {
+            throw new InvalidArgumentException('Response must contain some data.');
+        }
+
+        if (is_string($data)) {
+            $data = [[self::DATA_FIELD_MESSAGE => $data]];
+        }
+
+        if (isset($data[self::DATA_FIELD_MESSAGE])) {
+            $data = [$data];
+        }
+
+        foreach ($data as $error) {
+            if (false === is_array($error) || false === array_key_exists(self::DATA_FIELD_MESSAGE, $error)) {
+                throw new InvalidArgumentException(sprintf('Each error must contain a `%s` field.', self::DATA_FIELD_MESSAGE));
+            }
+        }
+
+        return new JsonResponse(
+            [
+                'status' => 'error',
+                'errors' => $data,
+            ],
+            $code,
+            $headers
+        );
+    }
+}
